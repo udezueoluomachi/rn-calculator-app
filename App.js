60fa357fdfddb504b487a3ba087563c033cec3a5
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-02-07 20:54:13 +0100

    :App interface setup ?

--- App.js
+++ App.js
@@ -1,18 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet ,Text, View , TextInput} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet , Text , View, Alert} from 'react-native';
 
-const btns = props => {}
 
 export default function App() {
+  const [screenValue, setScreenValue] = useState("0");
+  
+  const BBtn = props => {
+    return(
+      <View style={styles.BBtn} onpress={() => {
+        Alert.alert("0");
+        let str = screenValue.trim();
+        if(props.val == "=") {
+          setScreenValue(eval(str))
+        }
+        else {
+          str += props.val;
+          setScreenValue(str)
+        }
+      }}>
+        <Text style={styles.BtnText}>
+          {props.val}
+        </Text>
+      </View>
+    )
+  }
+
+  const SBtn = props => {
+    return (
+      <View style={[styles.SBtn, {
+        backgroundColor :  props.val == "Del" ? colors.red : colors.purple,
+        borderRadius : 10,
+        }]}>
+        <Text style={styles.BtnText}>
+          {props.val}
+        </Text>
+      </View>
+    )
+  }
   return (
-	<View style={styles.container}>
-	  <View style={styles.screen}>
-	  </View>
-	  <View style={styles.keysContainer}>
-	    <View style={[styles.LRKeysCont, styles.LKeysCont]}></View>
-	    <View style={[styles.LRKeysCont, styles.RKeysCont]}></View>
-	  </View>
-	</View>
+    <View style={styles.container}>
+      <View style={styles.screen}>
+        <Text style={{color : colors.white2, fontSize : 34}}>
+          {screenValue}
+        </Text>
+      </View>
+      <View style={styles.keysContainer}>
+        <View style={[styles.LRKeysCont, styles.LKeysCont]}>
+          <BBtn val="7"/>
+          <BBtn val="8"/>
+          <BBtn val="9"/>
+          <BBtn val="4"/>
+          <BBtn val="5"/>
+          <BBtn val="6"/>
+          <BBtn val="1"/>
+          <BBtn val="2"/>
+          <BBtn val="3"/>
+          <BBtn val="."/>
+          <BBtn val="0"/>
+          <BBtn val="="/>
+        </View>
+        <View style={[styles.LRKeysCont, styles.RKeysCont]}>
+          <SBtn val="Del"/>
+          <SBtn val="+"/>
+          <SBtn val="-"/>
+          <SBtn val="/"/>
+          <SBtn val="*"/>
+        </View>
+      </View>
+    </View>
   );
 }
 
@@ -40,22 +96,48 @@ const styles = StyleSheet.create({
     width: "100%",
     marginTop: 45,
     backgroundColor: colors.black2,
+    justifyContent : "center",
+    alignItems : "flex-end"
   },
   keysContainer: {
     width : "100%",
     height : "60%",
     marginTop : "15%",
-    backgroundColor : colors.red,
     flexWrap : "wrap"
   },
   LRKeysCont : {
     height : "100%",
-    backgroundColor : "#888",
+    justifyContent : "center",
+    alignItems : "center"
   },
   LKeysCont : {
     width : "75%",
+    flexDirection : "row",
+    flexWrap : "wrap"
   },
   RKeysCont : {
-    width : "25%"
+    width : "25%",
+    justifyContent : "space-evenly"
+  },
+  BBtn : {
+    height : "21%",
+    width : 65,
+    margin : "3%",
+    borderWidth : 2,
+    borderColor :  colors.purple,
+    borderRadius : 10,
+    justifyContent : "center",
+    alignItems : "center"
+  },
+  BtnText : {
+    fontSize : 30,
+    color : colors.white1
+  },
+  SBtn : {
+    height : "16%",
+    width : 65,
+    margin : "3%",
+    justifyContent : "center",
+    alignItems : "center"
   }
 });
